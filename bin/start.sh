5d64fc84778b7e4f8940eea69a5b4151c460c1ae
Giovani Del Nero Diniz <gdiniz@bbn.com>
2019-09-23 14:18:58 -0400

    fixed path for launch files

--- bin/start.sh
+++ bin/start.sh
@@ -1,59 +1,58 @@
 #!/bin/bash
 
-screen -d -m -S roscore $HOME/rover/src/rover_launcher/bin/start_roscore.sh
+screen -d -m -S roscore $HOME/rover/src/rover_launcher_sitl/bin/start_roscore.sh
 
 sleep 5
 
-screen -d -m -S nav_sat $HOME/rover/src/rover_launcher/bin/start_nav_sat.sh
+screen -d -m -S sitl $HOME/rover/src/rover_launcher_sitl/bin/start_gazebo_sitl.sh
 
 sleep 5
 
-screen -d -m -S icp $HOME/rover/src/rover_launcher/bin/start_lidar_icp.sh
+screen -d -m -S nav_sat $HOME/rover/src/rover_launcher_sitl/bin/start_nav_sat.sh
 
 sleep 5
 
-screen -d -m -S ekf_inertial $HOME/rover/src/rover_launcher/bin/start_ekf_inertial.sh
+screen -d -m -S icp $HOME/rover/src/rover_launcher_sitl/bin/start_lidar_icp.sh
 
 sleep 5
 
-screen -d -m -S bridge $HOME/rover/src/rover_launcher/bin/start_bridge.sh
+screen -d -m -S ekf_inertial $HOME/rover/src/rover_launcher_sitl/bin/start_ekf_inertial.sh
 
 sleep 5
 
-## This is not being used
-screen -d -m -S state_obs $HOME/rover/src/rover_launcher/bin/start_state_observer.sh
+screen -d -m -S bridge $HOME/rover/src/rover_launcher_sitl/bin/start_bridge.sh
 
 sleep 5
 
 ###################
 ## indoor nav stack
-screen -d -m -S nav_inertial $HOME/rover/src/rover_launcher/bin/start_nav_inertial.sh
+screen -d -m -S nav_inertial $HOME/rover/src/rover_launcher_sitl/bin/start_nav_inertial.sh
 
 sleep 5
 
-screen -d -m -S map_inertial $HOME/rover/src/rover_launcher/bin/start_map_inertial.sh
+screen -d -m -S map_inertial $HOME/rover/src/rover_launcher_sitl/bin/start_map_inertial.sh
 
 sleep 5
 
 ###################
 ## outdoor stack
 
-#screen -d -m -S gps $HOME/rover/src/rover_launcher/bin/start_gps_driver.sh
+#screen -d -m -S gps $HOME/rover/src/rover_launcher_sitl/bin/start_gps_driver.sh
 
 #sleep 15
 
-#screen -d -m -S gps_conv $HOME/rover/src/rover_launcher/bin/start_gps_converter.sh
+#screen -d -m -S gps_conv $HOME/rover/src/rover_launcher_sitl/bin/start_gps_converter.sh
 
 #sleep 5
 
-#screen -d -m -S gps_init $HOME/rover/src/rover_launcher/bin/start_gps_initializer.sh
+#screen -d -m -S gps_init $HOME/rover/src/rover_launcher_sitl/bin/start_gps_initializer.sh
 
 #sleep 5
 
-#screen -d -m -S ekf_global $HOME/rover/src/rover_launcher/bin/start_ekf_global.sh
+#screen -d -m -S ekf_global $HOME/rover/src/rover_launcher_sitl/bin/start_ekf_global.sh
 
 #sleep 5
 
-#screen -d -m -S nav_global $HOME/rover/src/rover_launcher/bin/start_nav_global.sh
+#screen -d -m -S nav_global $HOME/rover/src/rover_launcher_sitl/bin/start_nav_global.sh
 
 #sleep 5
--- bin/start_ekf_inertial.sh
+++ bin/start_ekf_inertial.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
 [ -f "/opt/ros/melodic/setup.sh" ] && source "/opt/ros/melodic/setup.sh"
-[ -f "/home/nvidia/rover/devel/setup.sh" ] && source "/home/nvidia/rover/devel/setup.sh"
+[ -f "$HOME/rover/devel/setup.sh" ] && source "$HOME/rover/devel/setup.sh"
 [ -f "/etc/default/ros" ] && source "/etc/default/ros"
 
 set -a
-source /home/nvidia/rover/src/rover_launcher/config/CCAST_CONFIG
+source $HOME/rover/src/rover_launcher/config/CCAST_CONFIG
 
-roslaunch /home/nvidia/rover/src/rover_launcher/launch/ekf_inertial.launch
+roslaunch $HOME/rover/src/rover_launcher/launch/ekf_inertial.launch
