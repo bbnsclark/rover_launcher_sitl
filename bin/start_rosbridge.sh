5d9e42820301b3644f1988d3a6d4d33b1080795a
Giovani Del Nero Diniz <gdiniz@bbn.com>
2019-11-22 13:58:51 -0500

    [FIX] included bbn bridge

--- bin/start_rosbridge.sh
+++ bin/start_rosbridge.sh
@@ -2,8 +2,10 @@
 
 [ -f "/opt/ros/melodic/setup.sh" ] && source "/opt/ros/melodic/setup.sh"
 [ -f "$HOME/rover/devel/setup.sh" ] && source "$HOME/rover/devel/setup.sh"
+[ -f "$HOME/rover/devel_isolated/setup.sh" ] && source "$HOME/rover/devel_isolated/setup.sh"
 [ -f "/etc/default/ros" ] && source "/etc/default/ros"
 
 set -a
+source $HOME/rover/src/rover_launcher/config/CCAST_CONFIG
 
 roslaunch rosbridge_server rosbridge_websocket.launch
