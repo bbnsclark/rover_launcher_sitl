7ff74cab173de00b5f4d3bd6569ec6b5c83571d2
Kerry Moffitt <kmoffitt@bbn.com>
2019-11-27 16:25:46 -0500

    AirSim GPS driver vs. Gazebo GpsDriver

---
+++ bin/start_state_observer_airsim.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+[ -f "/opt/ros/melodic/setup.sh" ] && source "/opt/ros/melodic/setup.sh"
+[ -f "$HOME/rover/devel/setup.sh" ] && source "$HOME/rover/devel/setup.sh"
+[ -f "/etc/default/ros" ] && source "/etc/default/ros"
+
+rosrun state_observer_sitl node.py _is_airsim:=True
