d2fe4a60b46761f8d34398466746b5c6be2abcb2
Giovani Del Nero Diniz <gdiniz@bbn.com>
2020-01-09 12:56:25 -0500

    [FIX] fixed tag detection definition source

--- bin/start_tag_detection.sh
+++ bin/start_tag_detection.sh
@@ -7,4 +7,4 @@
 set -a
 source $HOME/rover/src/rover_launcher/config/CCAST_CONFIG
 
-roslaunch $HOME/rover/src/apriltag_ros/apriltag_ros/launch/april_tag_detection.launch
+roslaunch $HOME/rover/src/rover_launcher_sitl/launch/april_tag_detection.launch
