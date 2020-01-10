f6760e2c55ed41b0114ea1637406e38f9c24d446
Giovani Del Nero Diniz <gdiniz@bbn.com>
2020-01-10 15:28:24 -0500

    [FIX] missing gazebo resource paths on startup script

--- bin/start_gazebo_sitl.sh
+++ bin/start_gazebo_sitl.sh
@@ -6,5 +6,6 @@
 [ -f "/etc/default/ros" ] && source "/etc/default/ros"
 
 export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:$HOME/rover/src/rover_gazebo/models/
+export GAZEBO_RESOURCE_PATH=$GAZEBO_RESOURCE_PATH:$HOME/rover/src/rover_gazebo/worlds/
 
 roslaunch rover_gazebo rover_gazebo.launch
