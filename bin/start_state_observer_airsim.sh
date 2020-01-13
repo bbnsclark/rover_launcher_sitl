07cc5578b118269f4eeec0506cc67271b180603e
Kerry Moffitt <kmoffitt@bbn.com>
2020-01-13 08:07:23 -0500

    pushd into state_observer_sitl, now that we're loading yaml from there

--- bin/start_state_observer_airsim.sh
+++ bin/start_state_observer_airsim.sh
@@ -4,4 +4,7 @@
 [ -f "$HOME/rover/devel/setup.sh" ] && source "$HOME/rover/devel/setup.sh"
 [ -f "/etc/default/ros" ] && source "/etc/default/ros"
 
+pushd ../../state_observer_sitl/src
 rosrun state_observer_sitl node.py _is_airsim:=True _platform_suffix:=_$1
+popd
+
