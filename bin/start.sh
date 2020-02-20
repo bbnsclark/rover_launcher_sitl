2d3fae057f1e369a2d6e04414edddd1448a28d4a
Giovani Del Nero Diniz <gdiniz@bbn.com>
2020-02-20 07:46:09 -0500

    [FIX] startup sequence

--- bin/start.sh
+++ bin/start.sh
@@ -15,19 +15,9 @@ echo '    - april tag detections'
 screen -d -m -S april_tags $HOME/rover/src/rover_launcher_sitl/bin/start_tag_detection.sh
 
 sleep 2
-echo '    - rosbridge'
-
-screen -d -m -S rosbridge $HOME/rover/src/rover_launcher/bin/start_rosbridge.sh
-
-sleep 2
 echo '    - kalman filter'
 
 screen -d -m -S ekf_inertial $HOME/rover/src/rover_launcher_sitl/bin/start_ekf_inertial.sh
-
-sleep 2
-echo '    - obstacle avoidance'
-
-screen -d -m -S avoidance $HOME/rover/src/rover_launcher_sitl/bin/start_avoidance.sh
 
 sleep 2
 echo '    - map_tf'
@@ -37,16 +27,20 @@ screen -d -m -S map_tf $HOME/rover/src/rover_launcher/bin/start_map_tf.sh
 sleep 2
 echo '    - navigation'
 
-screen -d -m -S navigation $HOME/rover/src/rover_launcher_sitl/bin/start_nav.sh
+screen -d -m -S navigation $HOME/rover/src/rover_launcher/bin/start_nav.sh
 
 sleep 2
 echo '    - state observer'
 
-screen -d -m -S state_obs $HOME/rover/src/rover_launcher_sitl/bin/start_state_observer.sh
+screen -d -m -S state_obs $HOME/rover/src/rover_launcher/bin/start_state_observer.sh
+
+sleep 5
+echo '    - rosbridge'
+
+screen -d -m -S rosbridge $HOME/rover/src/rover_launcher/bin/start_rosbridge.sh
 
 sleep 2
 echo '    - rviz'
 
 screen -d -m -S rviz $HOME/rover/src/rover_launcher_sitl/bin/start_rviz.sh
 
-sleep 2
