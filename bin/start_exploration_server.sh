1cdeafab8ee8d5ff3ea2257b7bea5b70ba4865bd
Giovani Del Nero Diniz <gdiniz@bbn.com>
2020-03-17 12:02:31 -0400

    [FIX] exploration launch script

--- bin/start_exploration_server.sh
+++ bin/start_exploration_server.sh
@@ -5,4 +5,4 @@
 [ -f "$HOME/rover/devel_isolated/setup.sh" ] && source "$HOME/rover/devel_isolated/setup.sh"
 [ -f "/etc/default/ros" ] && source "/etc/default/ros"
 
-roslaunch explore_lite explore_costmap.launch
+roslaunch exploration explore_costmap.launch
